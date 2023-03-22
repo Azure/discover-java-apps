69d6ec6a0d1266f0416c9fb3ab7652c1ca436018
zhiyongli <iamlizhiyong@outlook.com>
2023-03-22 14:24:15 +0800

    Add windows support

--- buildall.sh
+++ buildall.sh
@@ -1,15 +1,14 @@
-rm -rf ./output
+rm -rf ./bin
 GOOS=linux 
 GOARCH=amd64
 echo "Building discovery with GOOS=$GOOS GOARCH=$GOARCH"
-go build -mod=mod -o ./output/discovery-l cli/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -mod=mod -o ./bin/discovery-l cli/main.go
 
-GOOS=windows 
-CGO_ENABLED=1 
+CGO_ENABLED=0
+GOOS=windows
 CC=x86_64-w64-mingw32-gcc 
 CXX=x86_64-w64-mingw32-g++ 
 GOARCH=amd64
 echo "Building discovery with GOOS=$GOOS GOARCH=$GOARCH"
-go build -mod=mod -o ./output/discovery.exe cli/main.go
-chmod +x ./output/discovery.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -mod=mod -o ./bin/discovery.exe cli/main.go
 
