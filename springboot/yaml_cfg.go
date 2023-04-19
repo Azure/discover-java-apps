98f6c921d5464c25962ed129a5d2cc1224a604ee
qiaolei <qiaolei@microsoft.com>
2023-04-19 11:48:50 +0800

    feat: add modules for springboot discovery

--- springboot/yaml_cfg.go
+++ springboot/yaml_cfg.go
@@ -1,30 +1,5 @@
 package springboot
 
-// Server
-type Server struct {
-	Connect Connect `yaml:"connect"`
-}
-
-// Connect
-type Connect struct {
-	TimeoutSeconds int  `yaml:"timeoutSeconds"`
-	Parallel       bool `yaml:"parallel"`
-}
-
-// Pattern
-type Pattern struct {
-	App     []string `yaml:"app"`
-	Logging Logging  `yaml:"logging"`
-	Cert    []string `yaml:"cert"`
-	Static  Static   `yaml:"static"`
-}
-
-// Logging
-type Logging struct {
-	FilePatterns  []string      `yaml:"file_patterns"`
-	ConsoleOutput ConsoleOutput `yaml:"console_output"`
-}
-
 // ConsoleOutput
 type ConsoleOutput struct {
 	Patterns []string `yaml:"patterns"`
@@ -47,5 +22,30 @@ type YamlConfig struct {
 	Server  Server  `yaml:"server"`
 	Pattern Pattern `yaml:"pattern"`
 	Env     Env     `yaml:"env"`
+}
+
+// Server
+type Server struct {
+	Connect Connect `yaml:"connect"`
+}
+
+// Connect
+type Connect struct {
+	Parallel       bool `yaml:"parallel"`
+	TimeoutSeconds int  `yaml:"timeoutSeconds"`
+}
+
+// Pattern
+type Pattern struct {
+	App     []string `yaml:"app"`
+	Logging Logging  `yaml:"logging"`
+	Cert    []string `yaml:"cert"`
+	Static  Static   `yaml:"static"`
+}
+
+// Logging
+type Logging struct {
+	FilePatterns  []string      `yaml:"file_patterns"`
+	ConsoleOutput ConsoleOutput `yaml:"console_output"`
 }
 
