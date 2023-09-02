8afa3925da19fe484b61524fe890455c40260995
GitHub Action <action@github.com>
2023-09-02 20:08:21 +0800

    chroe: enable springboot app autostart

--- mock/start_java_app.sh
+++ mock/start_java_app.sh
@@ -8,8 +8,6 @@ if [ ! -f "$JAR_FILE" ]; then
     echo "$JAR_FILE does not exist, download from remote"
     curl -o $JAR_FILE https://raw.githubusercontent.com/Azure/discover-java-apps/main/mock/hellospring2x-0.0.1-SNAPSHOT.jar
 fi
-JAVA_OPTS_2x="-XX:InitialRAMPercentage=40.0 -XX:MaxRAMPercentage=40.0 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.local.only=true -Dmanagement.endpoints.jmx.exposure.include=healthmetrics -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DtestOption=abc=def -Dspring.jmx.enabled=true -Dserver.tomcat.mbeanregistry.enabled=true -Dfile.encoding=UTF8 -Dspring.config.import=optional:configserver:/ -Dspring.datasource.password=testpassword1234"
-nohup java $JAVA_OPTS_2x -jar $JAR_FILE > app.log 2>&1 &
 
 # start springboot app
 tee /home/migrateadmin/startApp.sh << END
