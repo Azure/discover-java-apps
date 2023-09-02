f269f056a17a58ce6947071af812fb27046181a0
GitHub Action <action@github.com>
2023-09-02 19:49:05 +0800

    chroe: enable springboot app autostart

--- mock/start_java_app.sh
+++ mock/start_java_app.sh
@@ -10,3 +10,25 @@ if [ ! -f "$JAR_FILE" ]; then
 fi
 JAVA_OPTS_2x="-XX:InitialRAMPercentage=40.0 -XX:MaxRAMPercentage=40.0 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.local.only=true -Dmanagement.endpoints.jmx.exposure.include=healthmetrics -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DtestOption=abc=def -Dspring.jmx.enabled=true -Dserver.tomcat.mbeanregistry.enabled=true -Dfile.encoding=UTF8 -Dspring.config.import=optional:configserver:/ -Dspring.datasource.password=testpassword1234"
 nohup java $JAVA_OPTS_2x -jar $JAR_FILE > app.log 2>&1 &
+
+# start springboot app
+tee /home/migrateadmin/startApp.sh << END
+#!/bin/bash
+
+JAVA_OPTS_2x="-XX:InitialRAMPercentage=40.0 -XX:MaxRAMPercentage=40.0 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.local.only=true -Dmanagement.endpoints.jmx.exposure.include=healthmetrics -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -DtestOption=abc=def -Dspring.jmx.enabled=true -Dserver.tomcat.mbeanregistry.enabled=true -Dfile.encoding=UTF8 -Dspring.config.import=optional:configserver:/ -Dspring.datasource.password=testpassword1234"
+java $JAVA_OPTS_2x -jar $JAR_FILE
+END
+
+chmod +x /home/migrateadmin/startApp.sh
+nohup /home/migrateadmin/startApp.sh 2>&1 &
+
+# Enable auto start
+tee -a /lib/systemd/system/springboot.service << END
+[Unit]
+Description=Start springboot application
+[Service]
+ExecStart=/home/migrateadmin/startApp.sh
+[Install]
+WantedBy=multi-user.target
+END
+systemctl enable startup.service --now
